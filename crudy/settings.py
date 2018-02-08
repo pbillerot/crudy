5ee539ea9b08f80d97ab251322fd36345f4b0561
pbillerot <philippe.billerot@gmail.com>
2018-02-08 18:28:10 +0100

    db sur home

--- crudy/settings.py
+++ crudy/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': '/mnt/sandisk/data/crudy/db.sqlite3',
+        'NAME': '/home/billerot/data/crudy/db.sqlite3',
     }
 }
 
